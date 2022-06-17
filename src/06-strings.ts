aa97ba7421245e0413823e7912d041f106681233
Enmanuel <sarmientoe@zippyttech.com>
2022-06-16 21:43:15 -0400

    how to work with strings

---
+++ src/06-strings.ts
@@ -0,0 +1,16 @@
+(() => {
+  let productTitle = 'My amazin product';
+  productTitle   = 'edit string';
+  console.log('productTitle', productTitle);
+
+  const description = "bla bla bla";
+  console.log('description', description);
+
+  const sumary = `
+  title: ${productTitle}
+  description: ${productTitle}
+  asd
+  `;
+  console.log('sumary', sumary);
+
+})();
